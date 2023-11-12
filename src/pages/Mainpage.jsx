0310854f92a17f76fc4896e6aa7805fa3a9f9f0b
ShinhyoBelok <shibo6937@gmail.com>
2023-11-11 23:56:18 -0400

    Add initial structure

---
+++ src/pages/Mainpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/navbar.css';
+import './styles/mainpage.css';
+
+
+export default function Mainpage() {
+  return (
+    <div className='main_page_container'>
+      MAIN PAGE CONTAINER
+    </div>
+  )
+}
